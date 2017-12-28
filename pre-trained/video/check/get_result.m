9f468b02e84afa5da1edd591c8d0e3e8a0ca589a
sx14 <1059363093@qq.com>
2017-12-28 23:05:06 +0800

    fix recall

--- pre-trained/video/check/get_result.m
+++ pre-trained/video/check/get_result.m
@@ -1,5 +1,5 @@
 % 获取单个视频的处理结果
-function [result,annotations] = get_result(video_dir,annotation_path,output_path, proposals, re_cal)
+function [result,annotations] = get_result(video_dir,annotation_path,output_path, proposals, time_cost)
 result_dir = 'result';
 if ~exist(fullfile(output_path, result_dir),'dir')
     mkdir(fullfile(output_path), result_dir);   % make result dir
@@ -7,9 +7,9 @@ end
 result_path = fullfile(output_path, result_dir);
 result_file_name = [video_dir '.mat'];
 [ground_truth_info, annotations] = annotation_xml_2_struct(annotation_path);
-if ~exist(fullfile(result_path, result_file_name),'file') || re_cal == true  % not done
+if ~exist(fullfile(result_path, result_file_name),'file')
     [recall,smT_IoU,hit] = cal_recall(ground_truth_info, annotations, proposals);
-    output_info = sprintf('Recall: %.2f%% smT_IoU: %.2f%% object_sum: %d candidate_sum: %d', recall * 100, smT_IoU * 100, size(hit,1),size(proposals,1));
+    output_info = sprintf('Recall: %.2f%% smT_IoU: %.2f%% object_sum: %d time: %.2f s', recall * 100, smT_IoU * 100, size(hit,1),time_cost);
     disp(output_info);
     for i = 1:size(hit,1)
         h = output_info(1,:);
@@ -19,6 +19,7 @@ if ~exist(fullfile(result_path, result_file_name),'file') || re_cal == true  % n
         disp(h);
         output_info = [output_info;h];
     end
+    result.time = time_cost;
     result.recall = recall;
     result.smT_IoU = smT_IoU;
     result.hit = hit;
--- pre-trained/video/main.m
+++ pre-trained/video/main.m
@@ -4,5 +4,5 @@ output_path = '/home/sunx/output/VMCG';
 annotation_base_path = '/home/sunx/dataset/ImageNet/Annotations';
 video_list = load('video_list.mat');
 video_list = video_list.video_list;
-% [recall, smT_IoU] = run([video_base_path,'/chosen0/'],'ILSVRC2015_train_00001000',[annotation_base_path,'/ILSVRC2015_VID_train_0000/'], output_path);
-run_all(video_base_path,video_list,output_path,annotation_base_path)
+[recall, smT_IoU] = run([video_base_path,'/chosen0/'],'ILSVRC2015_train_00001000',[annotation_base_path,'/ILSVRC2015_VID_train_0000/'], output_path);
+% run_all(video_base_path,video_list,output_path,annotation_base_path)
