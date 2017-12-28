7188270f7b50f3982d9437f5ab813520c86efffa
sx14 <1059363093@qq.com>
2017-12-28 23:56:52 +0800

    fix

--- pre-trained/video/run_all.m
+++ pre-trained/video/run_all.m
@@ -1,9 +1,9 @@
 function run_all(video_base_path,package_video,output_path,annotation_base_path)
-matlabpool 4;
+% matlabpool 4;
 parfor i = 1:size(package_video,1)
     video = package_video{i};
 %     annotation_package_path = fullfile(annotation_base_path,video.package_dir);
 %     video_package_path = fullfile(video_base_path,video.package_dir);
     run(video_base_path,video.video_dir,annotation_base_path,output_path);
 end
-matlabpool close;
+% matlabpool close;
