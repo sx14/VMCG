8e77f83e2ddcacab485ce31bf3133fceeeb88481
sx14 <1059363093@qq.com>
2017-12-28 15:35:29 +0800

    fix recall

--- pre-trained/video/check/get_result.m
+++ pre-trained/video/check/get_result.m
@@ -1,5 +1,5 @@
 % 获取单个视频的处理结果
-function [result,annotations] = get_result(video_dir,annotation_path,output_path, proposals, time_cost)
+function [result,annotations] = get_result(video_dir,annotation_path,output_path, proposals, re_cal)
 result_dir = 'result';
 if ~exist(fullfile(output_path, result_dir),'dir')
     mkdir(fullfile(output_path), result_dir);   % make result dir
@@ -7,14 +7,14 @@ end
 result_path = fullfile(output_path, result_dir);
 result_file_name = [video_dir '.mat'];
 [ground_truth_info, annotations] = annotation_xml_2_struct(annotation_path);
-if ~exist(fullfile(result_path, result_file_name),'file')  % not done
+if ~exist(fullfile(result_path, result_file_name),'file') || re_cal == true  % not done
     [recall,smT_IoU,hit] = cal_recall(ground_truth_info, annotations, proposals);
-    output_info = sprintf('Recall: %.2f%% smT_IoU: %.2f%% object_sum: %d time: %.2f s', recall * 100, smT_IoU * 100, size(hit,1),time_cost);
+    output_info = sprintf('Recall: %.2f%% smT_IoU: %.2f%% object_sum: %d candidate_sum: %d', recall * 100, smT_IoU * 100, size(hit,1),size(proposals,1));
     disp(output_info);
     for i = 1:size(hit,1)
         h = output_info(1,:);
         h(:) = ' ';
-        h_content = sprintf('%d : %.2f%%', i, hit(i,2)*100);
+        h_content = sprintf('%d : %.2f%%', i, hit(i,size(hit,2),2)*100);
         h(1:length(h_content)) = h_content;
         disp(h);
         output_info = [output_info;h];
@@ -22,7 +22,6 @@ if ~exist(fullfile(result_path, result_file_name),'file')  % not done
     result.recall = recall;
     result.smT_IoU = smT_IoU;
     result.hit = hit;
-    result.time = time_cost;
     % result dir: video_name.txt is readable; video_name.mat contains recall, smT-IoU, hit (object named result)
     save(fullfile(result_path, [video_dir,'.mat']),'result');
     output(result_path, [video_dir '.txt'], output_info, 'txt');
