ebd394eb31e6ebe9d042c6164bd60bf5ac8fc670
sx14 <1059363093@qq.com>
2018-01-04 15:03:46 +0800

    path

--- pre-trained/video/main.m
+++ pre-trained/video/main.m
@@ -1,7 +1,7 @@
 clear;clc;close all;
-video_base_path = '/home/sunx/dataset/ImageNet/train';
-output_path = '/home/sunx/output/VMCG';
-annotation_base_path = '/home/sunx/dataset/ImageNet/Annotations';
+video_base_path = '/media/magus/新加卷/ImageNet/train';
+output_path = '/home/magus/sunx/output/VMCG';
+annotation_base_path = '/media/magus/新加卷/ImageNet/Annotations';
 video_list = load('video_list.mat');
 video_list = video_list.video_list;
 % [recall, smT_IoU] = run([video_base_path,'/chosen0/'],'ILSVRC2015_train_00001000',[annotation_base_path,'/ILSVRC2015_VID_train_0000/'], output_path);
